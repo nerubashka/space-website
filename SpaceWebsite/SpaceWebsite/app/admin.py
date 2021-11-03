c5b0249b72ac8c138334ce4e1cfd9a78f231d050
stlavrukhina <lavrukhina.ad@gmail.com>
2021-11-03 19:55:27 +0300

    Event and EventCategory entities added

--- SpaceWebsite/SpaceWebsite/app/admin.py
+++ SpaceWebsite/SpaceWebsite/app/admin.py
@@ -1,4 +1,6 @@
-from .models import Event
+from .models import Event, EventCategory
 from django.contrib import admin
 
+admin.site.register(EventCategory)
 admin.site.register(Event)
+
--- SpaceWebsite/SpaceWebsite/app/models.py
+++ SpaceWebsite/SpaceWebsite/app/models.py
@@ -2,17 +2,31 @@
 Definition of models.
 """
 
-from django.db import models
 import json
 
-class Event (models.Model):
-    title = models.CharField(max_length = 100)
+from django.db import models
+
+
+class EventCategory(models.Model):
+    type = models.CharField(max_length=15, null=True)
+
+    def __str__(self):
+        return self.type
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    category = models.ForeignKey(EventCategory, on_delete=models.CASCADE, null=True, blank=False)
+    description = models.TextField(blank=False, null=True)
+    image = models.ImageField(upload_to='events_images', height_field=None, width_field=None,
+                              max_length=100, blank=True, null=True)
+    begin_date = models.DateField(blank=True, null=True)
+    end_data = models.DateField(blank=True, null=True)
+    begin_time = models.TimeField(blank=True, null=True)
+    end_time = models.TimeField(blank=True, null=True)
 
     def __str__(self):
         return self.title
 
     def tojson(self):
-        return json.dumps(self, default= lambda o: o.__dict__)
-
-
-# Create your models here.
+        return json.dumps(self, default=lambda o: o.__dict__)
